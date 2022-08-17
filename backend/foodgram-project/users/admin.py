d3ae14ea1ae3ec6645a470139d56c1026b614b6a
Pavel <web2cap@gmail.com>
2022-08-18 01:33:31 +0200

    admin users

--- backend/foodgram-project/users/admin.py
+++ backend/foodgram-project/users/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    """Add admin user managment"""
+
+    model = User
+    list_display = ("username", "is_staff", "is_superuser", "is_active")
+    list_filter = ("is_staff", "is_active", "is_superuser")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            ("Personal info"),
+            {"fields": ("first_name", "last_name", "email")},
+        ),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "password1",
+                    "password2",
+                    "email",
+                ),
+            },
+        ),
+    )
+    search_fields = ("username", "first_name", "last_name", "email")
+    ordering = ["username"]
+
+
+admin.site.register(User, CustomUserAdmin)
